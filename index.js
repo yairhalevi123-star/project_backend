1ceba8d73bd0877479e146b6e6c52cee0134d48d
yair halevi <yairhalevi123@gmail.com>
2025-12-03 10:43:52 +0200

    chang

--- index.js
+++ index.js
@@ -6,22 +6,19 @@ import morgan from "morgan";
 import path from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const absolutePath = path.join(__dirname, "..", "frontend", "index.html");
+const absolutePath1 = path.join(__dirname, "..", "frontend", "index2.html");
 const app = express();
 const port = 3000;
 
 function logger(req, res, next) {
   console.log("method : ", req.method);
-  console.log("method .url: ", res.url);
+  console.log("method .url: ", req.url);
   next();
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("combined"));
+app.use(morgan("tiny"));
 app.use(logger);
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/frontend/index.html");
-// });
-
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
@@ -31,6 +28,7 @@ app.post("/submit", (req, res) => {
   const name = req.body.name;
   const food = req.body.food;
   res.send(`<h1>hello ${name} your liked food is ${food}</h1>`).status(202);
+  // res.sendFile(absolutePath1);
 });
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
